db38b31f31ee26af589c183777464eb4d0cf7cbd
Camp Bill <139830735+soeos@users.noreply.github.com>
2025-07-24 23:52:22 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,11 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+console.log('自定义 JS 加载完成');
+
+(function () {
+  const script = document.createElement('script');
+  script.src = 'https://umami.ors.bio/script.js';
+  script.defer = true;
+  script.dataset.websiteId = '789b85a4-5038-4796-9d45-6927cc74992c';
+  document.body.appendChild(script);
+})();
+
